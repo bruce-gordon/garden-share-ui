3e47bd286be942e1c2d30dd1bf62380224445d28
Aaron Townsend <atownse17@gmail.com>
2021-02-19 15:11:58 -0600

    Create basic structure for login and Sign Up Co-authored-by: Bruce Gordon <68293135+bruce-gordon@users.noreply.github.com> Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com> Co-authored-by: Dani Coleman <danicoleman00@gmail.com> Co-authored-by: Garrett Cottrell <garrett.cottrell2116@gmail.com> Co-authored-by: Joshua Carey <jdcarey128@gmail.com>

---
+++ src/components/Login/Login.js
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from 'react'
+// import { useAuth } from "../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
+
+
+export default function Login() {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  // const { login } = useAuth()
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    try {
+      setError('')
+      setLoading(true)
+      // await login(emailRef.current.value, passwordRef.current.value)
+      // history.push("/archive")
+    } catch {
+      setError("Failed to find account")
+    }
+    setLoading(false)
+  }
+
+  return (
+    <>
+      <div>
+          <h2 className="text-center mb-4">Log In</h2>
+          {/* {error && <Alert variant="danger">{error}</Alert>} */}
+          <form onSubmit={handleSubmit}>
+            <div id="email">
+              <label>Email</label>
+              <input type="email" ref={emailRef} required />
+            </div>
+            <div id="password">
+              <label>Password</label>
+              <input type="password" ref={passwordRef} required />
+            </div>
+            <button disabled={loading} className="w-100" type="submit">Log In</button>
+          </form>
+      </div>
+      <div className="w-100 text-center mt-2">
+        Don't have an account? <Link to="/signup">Sign Up</Link>
+      </div>
+    </>
+  )
+}
+
+
