3feee0c4e3353a4720db278e151cd1aeffbac41d
Aaron Townsend <atownse17@gmail.com>
2021-02-20 18:04:13 -0600

    Build out product card Co-authored-by: Bruce Gordon <68293135+bruce-gordon@users.noreply.github.com> Co-authored-by: Chadrick Dickerson <12281987+chadrick-d-dev@users.noreply.github.com>

--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -1,3 +1,13 @@
-import React from 'react';
+import React from 'react'
+import './ProductCard.scss'
 
-export default ProductCard;
+export default function ProductCard({ produceName, type, quantity, unit, zipCode }) {
+  return (
+    <div className='productCard'>
+      <h3 className='cardHeader'>{ produceName }</h3>
+      <p>Type: { type }</p>
+      <p>{ quantity } { unit }</p>
+      <p>Zip Code: { zipCode }</p>
+    </div>
+  )
+}
