980508695c9c947f076ac34dced7caf41be31f39
Aaron Townsend <atownse17@gmail.com>
2021-02-20 18:03:54 -0600

    Add dropdown and Product rendering Co-authored-by: Bruce Gordon <68293135+bruce-gordon@users.noreply.github.com> Co-authored-by: Chadrick Dickerson <12281987+chadrick-d-dev@users.noreply.github.com>

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import mockData from '../productContainer/mockData'
+import ProductCard from '../productCard/ProductCard.js'
 
 export default function HomePage() {
   const [listingData, setListingData] = useState([])
@@ -9,15 +10,53 @@ export default function HomePage() {
     setListingData(mockData.data.getListings)
   })
 
-  const sortData = (products) => {
-    products.reduce((sortedData, product) => {
-      
-    }, [])
+  const getDropdownValues = (veggies) => {
+    return veggies.reduce((total, veggie) => {
+      const capitalVeggie = capitalizeLetters(veggie.produceName)
+      if (!total.includes(capitalVeggie)) {
+        total.push(capitalVeggie)
+      }
+      return total
+      }, [])
   }
+
+  const capitalizeLetters = (word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  const dropdownOptions = getDropdownValues(listingData).map(listing => {
+    return(
+      <option value={ listing }>
+        { listing }
+      </option>
+    )
+  })
+
+  const products = listingData.map(listing => {
+    return (
+      <ProductCard 
+        produceName={ listing.produceName } 
+        type={ listing.type }
+        quantity={ listing.quantity }
+        unit={ listing.unit }
+        zipCode={ listing.zipCode }
+      />
+    )
+  })
 
   return (
     <div>
       <h2>Available Produce In Your Area</h2>
+      <label htmlFor='vegetable-types'>Vegetable Types</label>
+      <select id='vegetable-types'>
+        <option value=''>
+          Select
+        </option>
+        { dropdownOptions }
+      </select>
+      <div>
+        { products }
+      </div>
     </div>
   )
 }
