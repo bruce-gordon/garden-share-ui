ec5ef7c88517380784c684a9e7627fad0a125355
Aaron Townsend <atownse17@gmail.com>
2021-02-22 17:52:27 -0600

    Build components and functions to handle rendering Co-authored-by: Bruce Gordon <68293135+bruce-gordon@users.noreply.github.com> Co-authored-by: Chadrick Dickerson <12281987+chadrick-d-dev@users.noreply.github.com>

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Profile.scss';
+import MyListings from '../myListings/MyListings.js'
 
 const Profile = () => {
   return (
-    <p>Profile</p>
+    <div>
+      <h2>User Name</h2>
+      <div>
+        <button>My Listings</button>
+        <button>My Offers</button>
+      </div>
+      <MyListings />
+      {/* {Conditional for rendering Listings/Offers Views} */}
+    </div>
   )
 }
 
--- src/components/userListing/userListing.js
+++ src/components/userListing/userListing.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import './UserListing.scss';
 
-const UserListing = () => {
+const UserListing = ({ id, updatedAt, produceType, produceName, quantity, units, offers }) => {
+
+  const allOffers = offers.map(offer => {
+    return(
+      <div style={{border: '3px solid green'}} id={ id } key={ id } >
+        <p>{ offer.updatedAt }</p>
+        <p>{ offer.produceType } { offer.produceName }</p>
+        <p>{ offer.quantity }</p>
+        <p>{ offer.units }</p>
+        { offer.status === 'pending' &&         
+        <div>
+          <button>Accept</button>
+          <button>Decline</button>
+        </div>
+        }
+      </div>
+    )
+  })
+
   return (
-    <p>UserListing</p>
+    <div>
+      <h4>Listing</h4>
+      <div style={{border: '3px solid red'}}>
+        <p>Date Posted</p>
+        <p>Listing Item</p>
+        <p>Quantity</p>
+        <p>Unit</p>
+      </div>
+      <div style={{border: '3px solid blue'}} id={ id }>
+        <p>{ updatedAt }</p>
+        <p>{ produceType } { produceName }</p>
+        <p>{ quantity }</p>
+        <p>{ units }</p>
+      </div>
+      <h4>Offers</h4>
+      <div style={{border: '3px solid yellow'}}>
+        <p>Date Posted</p>
+        <p>Listing Item</p>
+        <p>Quantity</p>
+        <p>Unit</p>
+      </div>
+      <div>
+        { allOffers }
+      </div>
+    </div>
   )
 }
 
