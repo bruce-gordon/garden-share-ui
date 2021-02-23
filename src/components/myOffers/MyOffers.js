550ecde99d498d05cdc3e90ddaf9a1cff17bde63
Aaron Townsend <atownse17@gmail.com>
2021-02-23 16:01:25 -0600

    Create MyOffers and UserOffer components Co-authored-by: Bruce Gordon <68293135+bruce-gordon@users.noreply.github.com>

--- src/components/myOffers/MyOffers.js
+++ src/components/myOffers/MyOffers.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import UserOffer from '../userOffer/UserOffer.js'
+import { connect, useDispatch } from 'react-redux'
+import { updateUserOffers } from '../../redux/actions/actions.js';
+import mockUserOffers from '../../mockData/mockUserOffer.js'
 
-export default function MyOffers() {
+
+const MyOffers = ({ userListings }) => {
+  
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    dispatch(updateUserOffers("userId", mockUserOffers.data.getUserOffers.listings))
+    // console.log(myOffers)
+    // console.log('filtered offers', filterOffers(myOffers, 'pending'))
+  }, [])
+
+  const filterListings = (listings, listingType) => {
+    return listings.filter(listing => {
+      if(listing.status === listingType) {
+        return listing
+      }
+    })
+  }
+
+  const openListings = filterListings(userListings, 'open').map(listing => {
+    console.log(listing)
+    return (
+      <UserOffer 
+        id={ listing.id }
+        key={ listing.id }
+        updatedAt={ listing.updatedAt }
+        produceType={ listing.produceType }
+        produceName={ listing.produceName }
+        quantity={ listing.quantity }
+        unit={ listing.unit }
+        status={ listing.status }
+        listingUser={ listing.user }
+        offers={ listing.offers }
+      />
+    )
+  })
+
+  const acceptedListings = filterListings(userListings, 'accepted').map(listing => {
+    console.log(listing)
+    return (
+      <UserOffer 
+        id={ listing.id }
+        key={ listing.id }
+        updatedAt={ listing.updatedAt }
+        produceType={ listing.produceType }
+        produceName={ listing.produceName }
+        quantity={ listing.quantity }
+        unit={ listing.unit }
+        status={ listing.status }
+        listingUser={ listing.user }
+        offers={ listing.offers }
+      />
+    )
+  })
+
   return (
     <div>
-      My Offers
+      <div style={{border: '5px solid black'}}>
+        <h3>My Open Offers</h3>
+        <div>
+          { openListings }
+        </div>
+      </div>
+      <div>
+        <h3>My Accepted Offers</h3>
+        <div style={{border: '5px solid purple'}}>
+          { acceptedListings }
+        </div>
+      </div>
     </div>
   )
 }
+
+const mapDispatchToProps = dispatch => ({
+  updateUserOffers: text => dispatch(updateUserOffers(text))
+})
+
+function myOffersState(state) {
+  return {
+    userListings: state.userOffers.offersList
+  }
+}
+
+export default connect(myOffersState, mapDispatchToProps)(MyOffers)
