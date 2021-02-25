3c38c175fd28fb5d3297aea27fb431609e282ebd
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-02-24 17:08:31 -0700

    ProductCard test change

--- src/components/productCard/ProductCard.test.js
+++ src/components/productCard/ProductCard.test.js
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import ProductCard from './ProductCard';
+import '@testing-library/jest-dom';
 
-test.skip('renders learn react link', () => {
-  render(
-    <ProductCard />
-  );
+describe('ProductCard', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <ProductCard />
+      </MemoryRouter>
+    )
+  })
 
-});
+  it.skip('should render product card', () => {
+    const productCard = screen.getByTestId('productCard')
+    expect(productCard).toBeInTheDocument()
+  })
+})
