64eeaa806afe11ff48642b9a85b0f75b17269db8
Bruce Gordon <brc.gordon@gmail.com>
2021-02-25 10:36:02 -0700

    Style the myOffers section in the Profile component

--- src/components/myOffers/MyOffers.js
+++ src/components/myOffers/MyOffers.js
@@ -6,9 +6,9 @@ import mockUserOffers from '../../mockData/mockUserOffer.js'
 
 
 const MyOffers = ({ userListings }) => {
-  
+
   const dispatch = useDispatch()
-  
+
   useEffect(() => {
     dispatch(updateUserOffers("userId", mockUserOffers.data.getUserOffers.listings))
     // console.log(myOffers)
@@ -26,7 +26,7 @@ const MyOffers = ({ userListings }) => {
   const openListings = filterListings(userListings, 'open').map(listing => {
     console.log(listing)
     return (
-      <UserOffer 
+      <UserOffer
         id={ listing.id }
         key={ listing.id }
         updatedAt={ listing.updatedAt }
@@ -44,7 +44,7 @@ const MyOffers = ({ userListings }) => {
   const acceptedListings = filterListings(userListings, 'accepted').map(listing => {
     console.log(listing)
     return (
-      <UserOffer 
+      <UserOffer
         id={ listing.id }
         key={ listing.id }
         updatedAt={ listing.updatedAt }
@@ -61,7 +61,7 @@ const MyOffers = ({ userListings }) => {
 
   return (
     <div>
-      <div style={{border: '5px solid black'}}>
+      <div className='my-listings'>
         <h3>My Open Offers</h3>
         <div>
           { openListings }
@@ -69,7 +69,7 @@ const MyOffers = ({ userListings }) => {
       </div>
       <div>
         <h3>My Accepted Offers</h3>
-        <div style={{border: '5px solid purple'}}>
+        <div className='my-listings'>
           { acceptedListings }
         </div>
       </div>
--- src/components/userOffer/UserOffer.js
+++ src/components/userOffer/UserOffer.js
@@ -5,13 +5,32 @@ const UserOffer = ({ id, updatedAt, produceName, produceType, quantity, unit, st
 
   const allOffers = offers.map(offer => {
     return(
-      <div style={{border: '3px solid green'}} id={ id } key={ id } >
-        <p>{ offer.updatedAt }</p>
-        <p>{ offer.produceType } { offer.produceName }</p>
-        <p>{ offer.quantity }</p>
-        <p>{ offer.unit }</p>
-        {offer.status === 'accepted' && 
-          <p>{ listingUser.firstName }</p>
+      <div
+        className='listing-offer'
+        id={ id }
+        key={ id }
+      >
+        <div className='sub-section' id={ id }>
+          <p className='sub-heading'>Date Posted</p>
+          <p>{ offer.updatedAt }</p>
+        </div>
+        <div className='sub-section' id={ id }>
+          <p className='sub-heading'>Listing Item</p>
+          <p>{ offer.produceType } { offer.produceName }</p>
+        </div>
+        <div className='sub-section' id={ id }>
+          <p className='sub-heading'>Quantity</p>
+          <p>{ offer.quantity }</p>
+        </div>
+        <div className='sub-section' id={ id }>
+          <p className='sub-heading'>Unit</p>
+          <p>{ offer.unit }</p>
+        </div>
+        {offer.status === 'accepted' &&
+          <div className='contact-info'>
+            <p className='sub-heading'>Gardener name:</p>
+            <p>{ listingUser.firstName }</p>
+          </div>
         }
       </div>
     )
@@ -19,31 +38,30 @@ const UserOffer = ({ id, updatedAt, produceName, produceType, quantity, unit, st
   })
 
   return (
-    <div>
-      <h4>Listing</h4>
-      <div style={{border: '3px solid red'}}>
-        <p>Date Posted</p>
-        <p>Listing Item</p>
-        <p>Quantity</p>
-        <p>Unit</p>
+    <div className='listing-section'>
+      <h4 className='section-heading'>Listing I offered on</h4>
+      <div className='user-listing'>
+        <div className='sub-section' id={ id }>
+          <p className='sub-heading'>Date Posted</p>
+          <p>{ updatedAt }</p>
+        </div>
+        <div className='sub-section' id={ id }>
+          <p className='sub-heading'>Listing Item</p>
+          <p>{ produceType } { produceName }</p>
+        </div>
+        <div className='sub-section' id={ id }>
+          <p className='sub-heading'>Quantity</p>
+          <p>{ quantity }</p>
+        </div>
+        <div className='sub-section' id={ id }>
+          <p className='sub-heading'>Unit</p>
+          <p>{ unit }</p>
+        </div>
       </div>
-      <div style={{border: '3px solid blue'}}>
-        <p>{ updatedAt }</p>
-        <p>{ produceType } { produceName }</p>
-        <p>{ quantity }</p>
-        <p>{ unit }</p>
-      </div>
-      <h4>Offers</h4>
-      <div style={{border: '3px solid yellow'}}>
-        <p>Date Posted</p>
-        <p>Listing Item</p>
-        <p>Quantity</p>
-        <p>Unit</p>
-        { status === 'accepted' && <p>Gardener</p>}
-      </div>
-      <div style={{border: '3px solid green'}}>
+      <h4 className='section-heading'>What I offered</h4>
+      <section className='listing-offer-section'>
         { allOffers }
-      </div>
+      </section>
     </div>
   )
 }
