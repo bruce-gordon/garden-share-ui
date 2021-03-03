c4d52f251cb3a31b5dc1b89781a1ae56df3f41f4
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-03-02 17:28:14 -0700

    Style Login Page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -59,16 +59,23 @@
 // export default Login;
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-
+import './Login.scss';
+import GardenShareLogo from '../../images/GardenShareLogo.png';
 
 const Login = () => {
   const { loginWithRedirect } = useAuth0()
 
   return (
-
-    <button onClick={ () => loginWithRedirect() }>
-      Log In
-    </button>
+    <div className='login'>
+      <h1 className='welcome-header'>Welcome To</h1>
+      <img className='logo' src={GardenShareLogo} />
+      <div className='login-container'>
+        <h2 className='login-header'>To Access Garden Share Please Log In</h2>
+        <button className='login-button' onClick={ () => loginWithRedirect() }>
+          Log In
+        </button>
+      </div>
+    </div>
   )
 }
 
